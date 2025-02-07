687b7386f713539c1a5b36786d52d9019b14ba9d
Vinay Singh Rawat <115731293+VinaySinghRawat@users.noreply.github.com>
2025-02-07 19:30:21 +0530

    Add files via upload

---
+++ springbootwebtutorial/src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/advices/ApiResponse.java
@@ -0,0 +1,29 @@
+package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.advices;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+@Data
+public class ApiResponse<T>{
+//	/@JsonFormat(pattern = "hh:mm:ss dd-MM-yyyy") 
+	  private LocalDateTime timeStamp;
+	    private T data;
+	    private ApiError error;
+
+	    public ApiResponse() {
+	        this.timeStamp = LocalDateTime.now();
+	    }
+
+	    public ApiResponse(T data) {
+	        this();
+	        this.data = data;
+	    }
+
+	    public ApiResponse(ApiError error) {
+	        this();
+	        this.error = error;
+	    }
+}
