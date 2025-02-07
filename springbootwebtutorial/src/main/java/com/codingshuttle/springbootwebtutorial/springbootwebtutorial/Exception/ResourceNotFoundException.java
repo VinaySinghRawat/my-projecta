c6955dfe7a3f905738271901008a0d520d98cc1e
Vinay Singh Rawat <115731293+VinaySinghRawat@users.noreply.github.com>
2025-02-07 19:29:13 +0530

    Delete springbootwebtutorial directory

--- springbootwebtutorial/src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/Exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.Exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
